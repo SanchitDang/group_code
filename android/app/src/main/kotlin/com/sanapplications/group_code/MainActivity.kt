193b208168e3c019fc0ac6dff0ccc49a7da79e81
Sanchit <sanchit.dang03@gmail.com>
2023-03-27 18:51:47 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/sanapplications/group_code/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sanapplications.group_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:241281680060:web:3e8adc5826b06f1ef906b9";
+  static String apiKey = "AIzaSyDHrqk6tj-fEW26z7EM9HxAhz08wdX3914";
+  static String messagingSenderId = "241281680060";
+  static String projectId = "groupcode-2743b";
+  final primaryColor = const Color(0xFFee7b64);
+}
